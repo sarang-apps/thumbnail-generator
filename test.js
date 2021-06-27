357ef8e95233bebad0c9d2c12ae76131d5bdefff
sarang-apps <baps..vach@gmail.com>
2021-06-27 11:06:44 +0530

    added audio art return

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var thumbnailGen = require('./index');
+var thumbnailGen = require("./index");
 const path = require("path");
 
 // var filePath = "/Users/computerroom/Desktop/test_files/job_queue.json";
@@ -9,7 +9,7 @@ const path = require("path");
 // var filePath = "/Users/computerroom/Desktop/test_files/test.jpg";
 // var filePath = "/Users/computerroom/Desktop/test_files/World ending.mp4";
 var filePath = "/Users/computerroom/Desktop/test_files/test.mp3";
-var filePath = "/Users/computerroom/Desktop/Screenshots/Output_Test/test1.jpg";
+// var filePath = "/Users/computerroom/Desktop/Screenshots/Output_Test/test1.jpg";
 
 var thumbsPath = "/Users/computerroom/Desktop/test_files/thumbnail.jpg";
 let thumbsFolder = "/Users/computerroom/Desktop/test_files/";
@@ -17,32 +17,36 @@ var spriteFolder = "/Users/computerroom/Desktop/test_files/";
 
 // TESTING
 (async () => {
-    
-    thumbnailGen.getFileType(filePath)
-    .then(type => {
+    thumbnailGen.getFileType(filePath).then((type) => {
         var fileName = path.basename(filePath, path.extname(filePath));
-        var thumbsPath = "/Users/computerroom/Desktop/test_files/thumbnail3.jpg";
+        var thumbsPath =
+            "/Users/computerroom/Desktop/test_files/thumbnail3.jpg";
 
-        if(type) {
+        if (type) {
             // console.log(type);
             switch (type) {
                 case "image":
-                    thumbnailGen.getPhotoThumbnail(filePath,thumbsPath);
+                    thumbnailGen.getPhotoThumbnail(filePath, thumbsPath);
                     break;
                 case "video":
-                    var thumbsPath = thumbnailGen.getVideoThumnail(filePath, thumbsFolder, fileName);
-                    var thumbsPath = thumbnailGen.getVideoSprite(filePath, spriteFolder, fileName);
+                    var thumbsPath = thumbnailGen.getVideoThumnail(
+                        filePath,
+                        thumbsFolder,
+                        fileName
+                    );
+                    var thumbsPath = thumbnailGen.getVideoSprite(
+                        filePath,
+                        spriteFolder,
+                        fileName
+                    );
                     break;
                 case "audio":
                     thumbnailGen.getAudioArt(filePath);
                     break;
-            
+
                 default:
                     break;
             }
-        };
+        }
     });
-    
-
-
-})()
+})();
