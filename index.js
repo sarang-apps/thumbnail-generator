64701b711d5f66d4d698da7be17b9a49c4c284f7
sarang-apps <baps..vach@gmail.com>
2021-06-27 18:19:43 +0530

    update video screenshots

--- index.js
+++ index.js
@@ -11,122 +11,119 @@ const audioMetadata = require("music-metadata");
 
 // Get File Type Function
 const getFileType = (path) => {
-  return new Promise((resolve, reject) => {
-    FileType.fromFile(path).then((fileType) => {
-      // console.log(fileType);
+	return new Promise((resolve, reject) => {
+		FileType.fromFile(path).then((fileType) => {
+			// console.log(fileType);
 
-      if (fileType) {
-        let fileString = fileType.mime;
-        let index = fileString.indexOf("/");
-        if (index > -1) {
-          let type = fileString.substring(0, index).trim();
-          resolve(type);
-        } else {
-          resolve(fileString);
-        }
-      } else {
-        let output = JSON.stringify({ status: "failed", error: e });
-        reject(output);
-      }
-    });
-  });
+			if (fileType) {
+				let fileString = fileType.mime;
+				let index = fileString.indexOf("/");
+				if (index > -1) {
+					let type = fileString.substring(0, index).trim();
+					resolve(type);
+				} else {
+					resolve(fileString);
+				}
+			} else {
+				let output = JSON.stringify({ status: "failed", error: e });
+				reject(output);
+			}
+		});
+	});
 };
 
 const getPhotoThumbnail = (path, thumbsPath) => {
-  return new Promise((resolve, reject) => {
-    try {
-      // console.log(thumbsPath);
-      sharp(path)
-        .resize(160)
-        .jpeg({
-          quality: 50,
-        })
-        .toFile(thumbsPath)
-        .then(() => {
-          let output = JSON.stringify({ status: "success" });
-          resolve(output);
-        });
-      // console.log(thumbsOutput);
-    } catch (e) {
-      let output = JSON.stringify({ status: "failed", error: e });
-      console.error("Thumbnail Generation Error: ", err);
-      reject(output);
-    }
-  });
+	return new Promise((resolve, reject) => {
+		try {
+			// console.log(thumbsPath);
+			sharp(path)
+				.resize(160)
+				.jpeg({
+					quality: 50,
+				})
+				.toFile(thumbsPath)
+				.then(() => {
+					let output = JSON.stringify({ status: "success" });
+					resolve(output);
+				});
+			// console.log(thumbsOutput);
+		} catch (e) {
+			let output = JSON.stringify({ status: "failed", error: e });
+			console.error("Thumbnail Generation Error: ", err);
+			reject(output);
+		}
+	});
 };
 
 const getVideoThumnail = (path, thumbsFolder, fileName) => {
-  return new Promise((resolve, reject) => {
-    try {
-      // let thumbsPath = thumbsFolder + fileName + "_thumbs";
+	return new Promise((resolve, reject) => {
+		try {
+			// let thumbsPath = thumbsFolder + fileName + "_thumbs";
 
-      ffmpeg(path)
-        .screenshots({
-          count: 1,
-          filename: fileName + "_thumbs",
-          folder: thumbsFolder,
-          size: "160x160",
-        })
-        .then(() => {
-          let output = JSON.stringify({ status: "success" });
-          resolve(output);
-        });
+			ffmpeg(path).screenshots({
+				count: 1,
+				filename: fileName + "_thumbs",
+				folder: thumbsFolder,
+				size: "160x160",
+			});
+			let output = JSON.stringify({ status: "success" });
+			resolve(output);
 
-      // console.log(metadata);
-    } catch (e) {
-      let output = JSON.stringify({ status: "failed", error: e });
-      reject(output);
-    }
-  });
+			// console.log(metadata);
+		} catch (e) {
+			let output = JSON.stringify({ status: "failed", error: e });
+			reject(output);
+		}
+	});
 };
 
 const getVideoSprite = (path, spriteFolder, fileName) => {
-  return new Promise((resolve, reject) => {
-    try {
-      let spritePath = spriteFolder + fileName + "sprite.jpg";
+	return new Promise((resolve, reject) => {
+		try {
+			let spritePath = spriteFolder + fileName + "sprite.jpg";
 
-      const metadata = sprite({
-        input: path,
-        output: spritePath,
-        width: 96,
-        cols: 5,
-        rows: 5,
-      }).then(() => {
-        let output = JSON.stringify({ status: "success" });
-        resolve(output);
-      });
+			const metadata = sprite({
+				input: path,
+				output: spritePath,
+				width: 96,
+				cols: 5,
+				rows: 5,
+			}).then(() => {
+				let output = JSON.stringify({ status: "success" });
+				resolve(output);
+			});
 
-      // console.log(metadata);
-    } catch (e) {
-      let output = JSON.stringify({ status: "failed", error: e });
-      reject(output);
-    }
-  });
+			// console.log(metadata);
+		} catch (e) {
+			let output = JSON.stringify({ status: "failed", error: e });
+			reject(output);
+		}
+	});
 };
 
 const getAudioArt = (path) => {
-  return new Promise((resolve, reject) => {
-    try {
-      audioMetadata.parseFile(path).then((metadata) => {
-        // console.log(metadata);
-        let audioArt = audioMetadata.selectCover(metadata.common.picture);
+	return new Promise((resolve, reject) => {
+		try {
+			audioMetadata.parseFile(path).then((metadata) => {
+				// console.log(metadata);
+				let audioArt = audioMetadata.selectCover(metadata.common.picture);
 
-        // console.log(audioArt);
+				// console.log(audioArt);
 
-        let output = JSON.stringify({ status: "success", thumbnail : audioArt});
-        resolve(output);
-      });
-    } catch (error) {
-      let output = JSON.stringify({ status: "failed", error: e });
-      reject(output);
-    }
-  });
+				let output = JSON.stringify({ status: "success", thumbnail: audioArt });
+				resolve(output);
+			});
+		} catch (error) {
+			let output = JSON.stringify({ status: "failed", error: e });
+			reject(output);
+		}
+	});
 };
 
 module.exports = {
-  getFileType,
-  getPhotoThumbnail,
-  getVideoThumnail,
-  getVideoSprite,
-  getAudioArt,
+	getFileType,
+	getPhotoThumbnail,
+	getVideoThumnail,
+	getVideoSprite,
+	getAudioArt,
 };
--- test.js
+++ test.js
@@ -7,8 +7,8 @@ const path = require("path");
 // var filePath = "/Users/computerroom/Desktop/test_files/WhatsApp Audio 2020-09-06 at 4.58.53 PM.mp4";
 // var filePath = "/Users/computerroom/Desktop/test_files/Mumay Smruty 3's.tif";
 // var filePath = "/Users/computerroom/Desktop/test_files/test.jpg";
-// var filePath = "/Users/computerroom/Desktop/test_files/World ending.mp4";
-var filePath = "/Users/computerroom/Desktop/test_files/test.mp3";
+var filePath = "/Users/computerroom/Desktop/test_files/World ending.mp4";
+// var filePath = "/Users/computerroom/Desktop/test_files/test.mp3";
 // var filePath = "/Users/computerroom/Desktop/Screenshots/Output_Test/test1.jpg";
 
 var thumbsPath = "/Users/computerroom/Desktop/test_files/thumbnail.jpg";
@@ -17,36 +17,36 @@ var spriteFolder = "/Users/computerroom/Desktop/test_files/";
 
 // TESTING
 (async () => {
-    thumbnailGen.getFileType(filePath).then((type) => {
-        var fileName = path.basename(filePath, path.extname(filePath));
-        var thumbsPath =
-            "/Users/computerroom/Desktop/test_files/thumbnail3.jpg";
+	thumbnailGen.getFileType(filePath).then((type) => {
+		var fileName = path.basename(filePath, path.extname(filePath));
+		var thumbsPath = "/Users/computerroom/Desktop/test_files/thumbnail3.jpg";
 
-        if (type) {
-            // console.log(type);
-            switch (type) {
-                case "image":
-                    thumbnailGen.getPhotoThumbnail(filePath, thumbsPath);
-                    break;
-                case "video":
-                    var thumbsPath = thumbnailGen.getVideoThumnail(
-                        filePath,
-                        thumbsFolder,
-                        fileName
-                    );
-                    var thumbsPath = thumbnailGen.getVideoSprite(
-                        filePath,
-                        spriteFolder,
-                        fileName
-                    );
-                    break;
-                case "audio":
-                    thumbnailGen.getAudioArt(filePath);
-                    break;
+		if (type) {
+			// console.log(type);
+			switch (type) {
+				case "image":
+					thumbnailGen.getPhotoThumbnail(filePath, thumbsPath);
+					break;
+				case "video":
+					var thumbsPath = thumbnailGen
+						.getVideoThumnail(filePath, thumbsFolder, fileName)
 
-                default:
-                    break;
-            }
-        }
-    });
+						.catch((error) => {
+							console.log(error);
+						});
+					var thumbsPath = thumbnailGen
+						.getVideoSprite(filePath, spriteFolder, fileName)
+						.catch((error) => {
+							console.log(error);
+						});
+					break;
+				case "audio":
+					thumbnailGen.getAudioArt(filePath);
+					break;
+
+				default:
+					break;
+			}
+		}
+	});
 })();
